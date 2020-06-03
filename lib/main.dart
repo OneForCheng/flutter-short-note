92a278f8cd9f47956c781af38e9625f92ba52b76
forcheng <2871979271@qq.com>
2020-06-03 20:27:26 +0800

    forcheng - add short note and show short note list

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,44 @@ class ShortNoteList extends StatefulWidget {
   createState() => new ShortNoteListState();
 }
 
+
 class ShortNoteListState extends State<ShortNoteList> {
+  List<String> _shortNoteItems = [];
+
+  void _addShortNoteItem() {
+    setState(() {
+      int index = _shortNoteItems.length;
+      _shortNoteItems.add('Item ' + index.toString());
+    });
+  }
+
+  Widget _buildShortNoteList() {
+    return new ListView.builder(
+       itemCount: _shortNoteItems.length,
+      itemBuilder: (context, index) {
+        return _buildShortNoteItem(_shortNoteItems[index]);
+      },
+    );
+  }
+
+  Widget _buildShortNoteItem(String shortNoteText) {
+    return new ListTile(
+      title: new Text(shortNoteText)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Todo List')
-      )
+        title: new Text('ShortNote List')
+      ),
+      body: _buildShortNoteList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addShortNoteItem,
+        tooltip: 'Add note',
+        child: new Icon(Icons.add)
+      ),
     );
   }
 }
