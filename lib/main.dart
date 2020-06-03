f5014c97566665eece94ac4ae6973148fc949a59
forcheng <2871979271@qq.com>
2020-06-03 18:00:33 +0800

    forcheng - add `ShortNoteList` with `StatefulWidget`

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,23 @@ class ShortNoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Short Note List',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Short Note List')
-        )
-      ),
+      home: new ShortNoteList(),
+    );
+  }
+}
+
+class ShortNoteList extends StatefulWidget {
+  @override
+  createState() => new ShortNoteListState();
+}
+
+class ShortNoteListState extends State<ShortNoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo List')
+      )
     );
   }
 }
